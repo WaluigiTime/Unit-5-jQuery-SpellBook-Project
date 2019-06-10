dca849edc7070a5f86dc486b073dcf76b5663284
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2019-06-10 09:42:09 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$(".fade").click(function() {
+    $(".spiderman").fadeOut();
+ var img;
+});
+
+
+
+//Spell #2
+
+$(".fade").click(function() {
+    $(".heroes").fadeOut();
+ var img;
+});
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
